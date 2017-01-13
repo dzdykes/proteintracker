1c3d2ab6013242a5ab82ea7ddc8ea11f86cf782a
dzdykes <dzdykes88@gmail.com>
2017-01-13 08:52:01 -0800

    began client side app

---
+++ ProteinTrackerClient/ProteinTrackerClient/ProteinTrackerForm.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace ProteinTrackerClient
+{
+    partial class ProteinTrackerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboSelectUser = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbGoal = new System.Windows.Forms.TextBox();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbAmount = new System.Windows.Forms.TextBox();
+            this.btnAddProtein = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblGoal = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboSelectUser
+            // 
+            this.cboSelectUser.FormattingEnabled = true;
+            this.cboSelectUser.Location = new System.Drawing.Point(16, 22);
+            this.cboSelectUser.Name = "cboSelectUser";
+            this.cboSelectUser.Size = new System.Drawing.Size(121, 21);
+            this.cboSelectUser.TabIndex = 0;
+            this.cboSelectUser.SelectedIndexChanged += new System.EventHandler(this.OnUserChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select a user";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Add new user";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Goal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Name";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(54, 80);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.TabIndex = 5;
+            // 
+            // tbGoal
+            // 
+            this.tbGoal.Location = new System.Drawing.Point(54, 106);
+            this.tbGoal.Name = "tbGoal";
+            this.tbGoal.Size = new System.Drawing.Size(100, 20);
+            this.tbGoal.TabIndex = 6;
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(16, 143);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(138, 31);
+            this.btnAddUser.TabIndex = 7;
+            this.btnAddUser.Text = "Add User";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.Click += new System.EventHandler(this.OnAddNewUser);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Amount";
+            // 
+            // tbAmount
+            // 
+            this.tbAmount.Location = new System.Drawing.Point(65, 19);
+            this.tbAmount.Name = "tbAmount";
+            this.tbAmount.Size = new System.Drawing.Size(100, 20);
+            this.tbAmount.TabIndex = 10;
+            // 
+            // btnAddProtein
+            // 
+            this.btnAddProtein.Location = new System.Drawing.Point(19, 45);
+            this.btnAddProtein.Name = "btnAddProtein";
+            this.btnAddProtein.Size = new System.Drawing.Size(101, 23);
+            this.btnAddProtein.TabIndex = 11;
+            this.btnAddProtein.Text = "Add Protein";
+            this.btnAddProtein.UseVisualStyleBackColor = true;
+            this.btnAddProtein.Click += new System.EventHandler(this.btnAddProtein_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(16, 83);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(34, 15);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Total";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 106);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 15);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Goal";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(86, 83);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(14, 15);
+            this.lblTotal.TabIndex = 14;
+            this.lblTotal.Text = "0";
+            // 
+            // lblGoal
+            // 
+            this.lblGoal.AutoSize = true;
+            this.lblGoal.Location = new System.Drawing.Point(86, 106);
+            this.lblGoal.Name = "lblGoal";
+            this.lblGoal.Size = new System.Drawing.Size(14, 15);
+            this.lblGoal.TabIndex = 15;
+            this.lblGoal.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbAmount);
+            this.groupBox1.Controls.Add(this.lblGoal);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblTotal);
+            this.groupBox1.Controls.Add(this.btnAddProtein);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Location = new System.Drawing.Point(199, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(187, 136);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Protein";
+            // 
+            // ProteinTrackerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 210);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnAddUser);
+            this.Controls.Add(this.tbGoal);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboSelectUser);
+            this.Name = "ProteinTrackerForm";
+            this.Text = "Protein Tracker";
+            this.Load += new System.EventHandler(this.ProteinTrackerForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboSelectUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbGoal;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbAmount;
+        private System.Windows.Forms.Button btnAddProtein;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblGoal;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ ProteinTrackerClient/ProteinTrackerClient/ProteinTrackerForm.cs
@@ -0,0 +1,55 @@
+﻿using ProteinTrackerClient.ProteinTrackerService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProteinTrackerClient
+{
+    public partial class ProteinTrackerForm : Form
+    {
+        private readonly ProteinTrackerServiceSoapClient service =
+            new ProteinTrackerServiceSoapClient();
+        private User[] users;
+
+        public ProteinTrackerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ProteinTrackerForm_Load(object sender, EventArgs e)
+        {
+            users = service.ListUsers();
+            cboSelectUser.DataSource = users;
+            cboSelectUser.DisplayMember = "Name";
+            cboSelectUser.ValueMember = "UserId";
+        }
+
+        private void OnAddNewUser(object sender, EventArgs e)
+        {
+            service.AddUser(tbName.Text, int.Parse(tbGoal.Text));
+            users = service.ListUsers();
+            cboSelectUser.DataSource = users;
+        }
+
+        private void OnUserChanged(object sender, EventArgs e)
+        {
+            var index = cboSelectUser.SelectedIndex;
+            lblTotal.Text = users[index].Total.ToString();
+            lblGoal.Text = users[index].Goal.ToString();
+        }
+
+        private void btnAddProtein_Click(object sender, EventArgs e)
+        {
+            var userId = users[cboSelectUser.SelectedIndex].UserId;
+            var newTotal = service.AddProtien(int.Parse(tbAmount.Text), userId);
+            users[cboSelectUser.SelectedIndex].Total = newTotal;
+            lblTotal.Text = newTotal.ToString();
+        }
+    }
+}
